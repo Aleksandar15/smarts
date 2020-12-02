f21bfab2cf86812353ac7dd5e514fcac243881d4
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2020-12-02 11:51:19 +0100

    Updates

---
+++ precache-manifest.5f5f36b708f5b4ef39b35e0c1d074623.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dd2e75379712fa2449cf10644a519c",
+    "url": "/smarts/index.html"
+  },
+  {
+    "revision": "6503f1d668d0d98f073e",
+    "url": "/smarts/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "704e9b6efc41ed2bd63d",
+    "url": "/smarts/static/css/main.b8f9097d.chunk.css"
+  },
+  {
+    "revision": "6503f1d668d0d98f073e",
+    "url": "/smarts/static/js/2.87c31eb5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/smarts/static/js/2.87c31eb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "704e9b6efc41ed2bd63d",
+    "url": "/smarts/static/js/main.a3705eef.chunk.js"
+  },
+  {
+    "revision": "fe61445e6cdb01d91ecf",
+    "url": "/smarts/static/js/runtime-main.8e6602b2.js"
+  }
+]);
